3e0984abd4f5ffaa1ff9c3de8b8b67a5c17a14db
akvlad <akvlad90@gmail.com>
2023-06-14 11:41:26 +0300

    rm vendor & update bigcache tests

--- bigcache_timing_test.go
+++ bigcache_timing_test.go
@@ -1,33 +1,116 @@
 package fastcache
 
 import (
+	"sync"
 	"testing"
 )
 
+const items = 100
+
 func BenchmarkSetBig(b *testing.B) {
-	key := uint64(12345)
-	value := createValue(256*1024, 0)
+	value := createValue(8*1024*1024, 0)
 	c := New(1024 * 1024)
 	b.SetBytes(int64(len(value)))
 	b.ReportAllocs()
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			c.SetBig(key, value)
+			for i := uint64(0); i < items; i++ {
+				c.SetBig(i, value)
+			}
 		}
 	})
 }
 
 func BenchmarkGetBig(b *testing.B) {
 	key := uint64(12345)
-	value := createValue(265*1024, 0)
+	value := createValue(8*1024*1024, 0)
 	c := New(1024 * 1024)
 	c.SetBig(key, value)
 	b.SetBytes(int64(len(value)))
 	b.ReportAllocs()
 	b.RunParallel(func(pb *testing.PB) {
-		var buf []byte
 		for pb.Next() {
-			buf = c.GetBig(buf[:0], key)
+			for i := uint64(0); i < items; i++ {
+				_ = c.GetBig(nil, 12345)
+			}
+		}
+	})
+}
+
+func BenchmarkGetSetBig(b *testing.B) {
+	key := uint64(12345)
+	value := createValue(8*1024*1024, 0)
+	c := New(1024 * 1024)
+	c.SetBig(key, value)
+	b.SetBytes(int64(len(value)))
+	b.ReportAllocs()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i := uint64(0); i < items; i++ {
+				c.SetBig(i, value)
+			}
+			for i := uint64(0); i < items; i++ {
+				_ = c.GetBig(nil, i)
+			}
+		}
+	})
+}
+
+func BenchmarkGetBigMap(b *testing.B) {
+	key := uint64(12345)
+	value := createValue(8*1024*1024, 0)
+	c := make(map[uint64][]byte)
+	c[key] = value
+	mtx := &sync.Mutex{}
+	b.SetBytes(int64(len(value)))
+	b.ReportAllocs()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i := uint64(0); i < items; i++ {
+				mtx.Lock()
+				_ = c[key]
+				mtx.Unlock()
+			}
+		}
+	})
+
+}
+
+func BenchmarkSetBigMap(b *testing.B) {
+	value := createValue(8*1024*1024, 0)
+	mtx := sync.Mutex{}
+	c := map[uint64][]byte{}
+	b.SetBytes(int64(len(value)))
+	b.ReportAllocs()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i := uint64(0); i < items; i++ {
+				mtx.Lock()
+				c[i] = value
+				mtx.Unlock()
+			}
+		}
+	})
+}
+
+func BenchmarkGetSetBigMap(b *testing.B) {
+	value := createValue(8*1024*1024, 0)
+	mtx := sync.Mutex{}
+	c := map[uint64][]byte{}
+	b.SetBytes(int64(len(value)))
+	b.ReportAllocs()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			for i := uint64(0); i < items; i++ {
+				mtx.Lock()
+				c[i] = value
+				mtx.Unlock()
+			}
+			for i := uint64(0); i < items; i++ {
+				mtx.Lock()
+				_ = c[i]
+				mtx.Unlock()
+			}
 		}
 	})
 }
